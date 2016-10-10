1884f8120751c94d79a3794a9b5452cd65e4a5e6
Patrick Kubiak <patrick@antuple.net>
2016-10-10 15:12:24 -0400

    stand-alone article starter

--- assets/app.js
+++ assets/app.js
@@ -7,33 +7,15 @@ exampleApp.controller('appCtrl', function($scope, $location) {
 
   // current path
   $scope.location = $location;
-
-  // close drawer
-  $scope.closeDrawer = function() {
-    document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible');
-    document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible');
-  }
 });
 
 // page routing
 exampleApp.config(function($routeProvider) {
   $routeProvider
 
-  // default redirect to home page
+  // default article page
   .when('/', {
-    redirectTo: '/home',
-  })
-
-  // route for the home page
-  .when('/home', {
-    controller: 'homeController',
-    templateUrl: 'assets/pages/home.tmpl.html'
-  })
-
-  // route for the demo page
-  .when('/demo', {
-    controller: 'demoController',
-    templateUrl: 'assets/pages/demo.tmpl.html'
+    templateUrl: 'assets/pages/article.tmpl.html'
   })
 
   // default redirect
@@ -41,26 +23,3 @@ exampleApp.config(function($routeProvider) {
     redirectTo: '/'
   });
 });
-
-exampleApp.controller('homeController', function($scope) {
-  // controller for the home view
-});
-
-exampleApp.controller('demoController', function($scope) {
-  // controller for the demo view
-});
-
-exampleApp.directive('demo', function () {
-  function link(scope, element, attrs) {
-    // register DOM listeners and update DOM here
-  }
-
-  return {
-    restrict: 'E',
-    templateUrl: 'assets/templates/demo.tmpl.html',
-    scope: {
-      test: '@'
-    },
-    link: link
-  }
-})
